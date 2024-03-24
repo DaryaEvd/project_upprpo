4bec4144efc0a491d4354dd8aa682e5d2fa85691
Стещенко Анастасия Романовна <a.steshchenko@g.nsu.ru>
2024-03-25 00:19:25 +0700

    added format

--- PseudoScene/src/main/java/ru/nsu/fit/capibaras/shapes/ShapeCreatingException.java
+++ PseudoScene/src/main/java/ru/nsu/fit/capibaras/shapes/ShapeCreatingException.java
@@ -1,12 +1,15 @@
 package ru.nsu.fit.capibaras.shapes;
 
+import java.text.DecimalFormat;
+
 public class ShapeCreatingException extends Exception {
     private ShapeCreatingException(String message) {
         super(message);
     }
 
     static ShapeCreatingException duplicateOption(double firstSide, double secondSide, double thirdSide) {
-        return new ShapeCreatingException(String.format("Triangle with parameters: %f, %f, %f doesn't exist",
-                firstSide, secondSide, thirdSide));
+        DecimalFormat df = new DecimalFormat("#.##");
+        return new ShapeCreatingException(String.format("Triangle with parameters: %s, %s, %s doesn't exist",
+                df.format(firstSide), df.format(secondSide), df.format(thirdSide)));
     }
 }
