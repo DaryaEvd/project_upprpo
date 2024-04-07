30179ba5586442071fcd66cf0611313e15b9eb39
n.kotelnikova1 <n.kotelnikova1@g.nsu.ru>
2024-04-07 21:59:00 +0700

    fix writing error in api.js

--- client/js/api.js
+++ client/js/api.js
@@ -1,17 +1,37 @@
-export async function getRequest(path) {
-  return fetch(`http://109.71.242.166:8080/shape/${path}`, { method: "GET", })
-    .then((response) => {
-      return response.json();
-    })
-    .catch((e) => alert("Getting error: " + e.message));
+import { getClientId } from "./utils.js";
+
+export function getRequest(path) {
+  const clientId = getClientId();
+  const response = fetch(`http://109.71.242.166:8080/shape/${path}/${clientId}`, { method: "GET", })
+    .then((response) => response.json())
+    .catch((e) => alert("Getting error: " + e));
+  if (!response) {
+    throw new Error();
+  }
+  return response;
 }
 
-export async function postRequest(path, params) {
-  await fetch(`http://109.71.242.166:8080/shape/${path}` + params, {
-    method: "POST",
-  });
+export async function postRequest(path, queryParams) {
+  const searchParams = new URLSearchParams(queryParams);
+  searchParams.append('client_id', getClientId());
+  const response = await fetch(
+    `http://109.71.242.166:8080/shape/${path}?` + searchParams,
+    { method: "POST" }
+  ).catch((e) => alert(`Error: ${e.message}`));
+  console.log("#", response);
+  if (!response) {
+    throw new Error();
+  }
+  if (!response.ok) {
+    const data = await response.json();
+    alert(data.message);
+    throw new Error(data.message);
+  }
+  return response;
 }
 
-export async function deleteRequest(path, params) {
-  await fetch(`http://109.71.242.166:8080/shape/${path}?` + params, { method: "DELETE", });
+export async function deleteRequest(path) {
+  const params = new URLSearchParams({'client_id': getClientId()})
+  await fetch(`http://109.71.242.166:8080/shape/${path}?` + params, { method: "DELETE", })
+    .catch((e) => alert("Getting error: " + e));
 }
--- client/js/apiShape.js
+++ client/js/apiShape.js
@@ -1,104 +1,59 @@
-import { getClientId } from './utils.js';
 import { getRequest, postRequest, deleteRequest } from './api.js';
 
 export const API = {
   sendRequestToGetShapes: () => {
-    const clientId = getClientId();
-    return getRequest(`chosen/shapes_list/${clientId}`);
+    return getRequest(`chosen/shapes_list`);
   },
   sendRequestToGetShapesName: () => {
-    const clientId = getClientId();
-    return getRequest(`chosen/names_list/${clientId}`);
+    return getRequest(`chosen/names_list`);
   },
   sendRequestToCreateCircle: async (params) => {
-    const clientId = getClientId();
-    try {
-      postRequest('circle?', new URLSearchParams({ 'client_id': clientId, 'radius': params.radius, }),)
-      alert("The circle has been successfully created!");
-    } catch (e) {
-      return alert(`Circle creation error: ${e}`);
-    }
+    await postRequest('circle', params)
+    alert("The circle has been successfully created!");
   },
   sendRequestToCreateEllipse: async (params) => {
-    const clientId = getClientId();
-    try {
-      postRequest('ellipse?', new URLSearchParams({ 'client_id': clientId, 'major_axis': params.major_axis, 'minor_axis': params.minor_axis, }));
-      alert("The ellipse has been successfully created!");
-    } catch (e) {
-      return alert(`Ellipse creation error: ${e}`);
-    }
+    await postRequest('ellipse', {
+      'major_axis': params.major_axis,
+      'minor_axis': params.minor_axis,
+    });
+    alert("The ellipse has been successfully created!");
   },
   sendRequestToCreateSquare: async (params) => {
-    const clientId = getClientId();
-    try {
-      postRequest('square?', new URLSearchParams({ 'client_id': clientId, 'side': params.side, }));
-      alert("The square has been successfully created!");
-    } catch (e) {
-      return alert(`Square creation error: ${e}`);
-    }
+    await postRequest('square', { 'side': params.side, });
+    alert("The square has been successfully created!");
   },
   sendRequestToCreateRectangle: async (params) => {
-    const clientId = getClientId();
-    try {
-      postRequest('rectangle?', new URLSearchParams({
-        'client_id': clientId,
-        'side': params.side,
-        'base': params.base,
-      }))
-      alert("The rectangle has been successfully created!");
-    } catch (e) {
-      return alert(`Rectangle creation error: ${e}`);
-    }
+    await postRequest('rectangle', {
+      'side': params.side,
+      'base': params.base,
+    })
+    alert("The rectangle has been successfully created!");
   },
   sendRequestToCreateParallelogram: async (params) => {
-    const clientId = getClientId();
-    try {
-      postRequest('parallelogram?', new URLSearchParams({
-        'client_id': clientId,
-        'side': params.side,
-        'base': params.base,
-        'angle': params.angle,
-      }));
-      alert("The parallelogram has been successfully created!");
-    } catch (e) {
-      return alert(`Parallelogram creation error: ${e}`);
-    }
+    await postRequest('parallelogram', {
+      'side': params.side,
+      'base': params.base,
+      'angle': params.angle,
+    });
+    alert("The parallelogram has been successfully created!");
   },
   sendRequestToCreateRhombus: async (params) => {
-    const clientId = getClientId();
-    try {
-      postRequest('rhombus?', new URLSearchParams({
-        'client_id': clientId,
-        'side': params.side,
-        'angle': params.angle,
-      }));
-      alert("The rhombus has been successfully created!");
-    } catch (e) {
-      return alert(`Rhombus creation error: ${e}`);
-    }
+    await postRequest('rhombus', {
+      'side': params.side,
+      'angle': params.angle,
+    });
+    alert("The rhombus has been successfully created!");
   },
   sendRequestToCreateTriangle: async (params) => {
-    const clientId = getClientId();
-    try {
-      postRequest('triangle?', new URLSearchParams({
-        'client_id': clientId,
-        'first_side': params.first_side,
-        'second_side': params.second_side,
-        'third_side': params.third_side,
-      }));
-      alert("The triangle has been successfully created!");
-    } catch (e) {
-      return alert(`Triangle creation error: ${e}`);
-    }
+    await postRequest('triangle', {
+      'first_side': params.first_side,
+      'second_side': params.second_side,
+      'third_side': params.third_side,
+    });
+    alert("The triangle has been successfully created!");
   },
   sendRequestToDeleteShape: async (shapeType) => {
-    const clientId = getClientId();
-    try {
-      deleteRequest(shapeType, new URLSearchParams({'client_id': clientId,}));
-      alert(`The ${shapeType} has been successfully deleted!`);
-    } catch (e) {
-      alert(`${shapeType} delete error: ${e}`);
-      throw e;
-    }
+    await deleteRequest(shapeType);
+    alert(`The ${shapeType} has been successfully deleted!`);
   },
 };
