ba0b758a876b4a30b8c273528d17445b70d29c35
n.kotelnikova1 <n.kotelnikova1@g.nsu.ru>
2024-04-07 22:00:00 +0700

    Deleted delete.js

--- client/js/delete.js
+++
@@ -1,8 +0,0 @@
-import { API } from './api.js';
-
-export function addEventDelete(wrapper,  elem, shapeType) {
-    elem.addEventListener('click', async function () {
-        await API.sendRequestToDeleteShape(shapeType);
-        wrapper.removeChild(elem);
-    })
-};
