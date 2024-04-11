cd3efc3444baede8016c8e0f4282473052526fa2
n.kotelnikova1 <n.kotelnikova1@g.nsu.ru>
2024-04-11 23:20:18 +0700

    Commit & sync

--- client/js/api.js
+++ client/js/api.js
@@ -2,7 +2,7 @@ import { getClientId } from "./utils.js";
 
 export function getRequest(path) {
   const clientId = getClientId();
-  const response = fetch(`http://109.71.242.166:8080/shape/${path}/${clientId}`, { method: "GET", })
+  const response = fetch(`http://localhost:8080/shape/${path}/${clientId}`, { method: "GET", })
     .then((response) => response.json())
     .catch((e) => alert("Getting error: " + e));
   if (!response) {
@@ -15,7 +15,7 @@ export async function postRequest(path, queryParams) {
   const searchParams = new URLSearchParams(queryParams);
   searchParams.append('client_id', getClientId());
   const response = await fetch(
-    `http://109.71.242.166:8080/shape/${path}?` + searchParams,
+    `http://localhost:8080/shape/${path}?` + searchParams,
     { method: "POST" }
   ).catch((e) => alert(`Error: ${e.message}`));
   console.log("#", response);
@@ -32,6 +32,6 @@ export async function postRequest(path, queryParams) {
 
 export async function deleteRequest(path) {
   const params = new URLSearchParams({'client_id': getClientId()})
-  await fetch(`http://109.71.242.166:8080/shape/${path}?` + params, { method: "DELETE", })
+  await fetch(`http://localhost:8080/shape/${path}?` + params, { method: "DELETE", })
     .catch((e) => alert("Getting error: " + e));
 }
